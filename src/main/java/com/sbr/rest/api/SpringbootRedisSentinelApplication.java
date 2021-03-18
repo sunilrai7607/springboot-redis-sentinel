71bc606f066fb1564de0e0ca11e3519c75ae8b19
sunilrai <sunilrai7607@gmail.com>
2021-03-17 22:43:26 -0400

    initial commit

---
+++ src/main/java/com/sbr/rest/api/SpringbootRedisSentinelApplication.java
@@ -0,0 +1,13 @@
+package com.sbr.rest.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRedisSentinelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRedisSentinelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sbr/rest/api/SpringbootRedisSentinelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbr.rest.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRedisSentinelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
